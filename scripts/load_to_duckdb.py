1d22fff726d1c2b60c7e32f7440645e594d5f961
kyerrapragada <kashacad@gmail.com>
2025-10-24 00:19:53 -0500

    bronze layer loaded and tested

--- scripts/load_to_duckdb.py
+++ scripts/load_to_duckdb.py
@@ -1,16 +1,17 @@
 import glob, duckdb, polars as pl
 from pathlib import Path
 
-db_path = "data/weather.duckdb"
+db_path = "weather.duckdb"
 raw_dir = Path("data/raw")
-latest = max(glob.glob(str(raw_dir / "weather_*.csv")))
+latest = max(glob.glob(str(raw_dir / "weather_global_history_20251013_232134.csv")))
 con = duckdb.connect(db_path)
 con.execute("CREATE SCHEMA IF NOT EXISTS raw")
 df = pl.read_csv(latest)
 con.execute("""
-    CREATE TABLE IF NOT EXISTS raw.weather (
-        city TEXT, date DATE, temp_max DOUBLE, temp_min DOUBLE, precipitation DOUBLE
+    CREATE TABLE IF NOT EXISTS raw.weatherhistorical (
+        city TEXT, date DATE, temp_max DOUBLE, temp_min DOUBLE, precipitation DOUBLE, windspeed_max DOUBLE
     )
 """)
-con.execute("INSERT INTO raw.weather SELECT * FROM df")
-print(f"Loaded {latest} into raw.weather")
+con.register("df", df)
+con.execute("INSERT INTO raw.weatherhistorical SELECT city, date, temp_max, temp_min, precipitation, windspeed_max FROM df")
+print(f"Loaded {latest} into raw.weatherhistorical")
